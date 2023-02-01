d8aa1c6600b2fd365f36994a9228d786deb395bd
Joao Madeiras <joaohrpereira@gmail.com>
2023-02-01 13:27:03 +0000

    Fix declaration types

---
+++ dist/index.d.ts
@@ -0,0 +1,12 @@
+/**
+ * Get the fib retracement based on `0` and `1` fib retracement levels.
+ */
+export declare const getFibRetracement: ({ levels }: {
+    levels: Record<number | string, number | string>;
+}) => {
+    [k: string]: any;
+};
+/**
+ * Levels.
+ */
+export declare const levels: number[];
